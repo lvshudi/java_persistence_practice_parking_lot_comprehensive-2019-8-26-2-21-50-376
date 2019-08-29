3a59e0cfcf2520d70c0392e4075143f901a96b83
lvshudi <lvshudi119@126.com>
2019-08-29 22:39:50 +0800

    test is not pass

--- src/test/java/tws/repository/EmployeeMapperTest.java
+++ src/test/java/tws/repository/EmployeeMapperTest.java
@@ -5,9 +5,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.jdbc.JdbcTestUtils;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
 import tws.entity.ParkingBoy;
 
 import javax.sql.DataSource;
@@ -18,6 +24,16 @@ import static org.junit.Assert.*;
 @RunWith(SpringRunner.class)
 @MybatisTest
 public class EmployeeMapperTest {
+	@Autowired
+	private MockMvc mockMvc;
+
+	//postman测试没有问题，但是这个启动不了
+	@Test
+	public void should_return_list_when_query() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/parkingboy")).andExpect(MockMvcResultMatchers.status().isOk())
+				.andDo(MockMvcResultHandlers.print()).andReturn();
+	}
+
 
   
 }
